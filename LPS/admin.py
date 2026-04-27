283508a0dd1b2da849e2565c724943d55e9af5c5
TheNerdVeVo <joshuajimenez55432@gmail.com>
2026-04-27 15:22:42 -0500

    Update admin.py

--- LPS/admin.py
+++ LPS/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import LotteryTicket, Order, ElectronicTicket, CustomerProfile, LotteryDraw
+from .models import LotteryTicket, Order, ElectronicTicket, CustomerProfile, Notification, LotteryDraw
 
-##==================================================
-# These are the different databases in Django admin
-#===================================================
+##==================================================================
+# These are the different databases in Django admin to keep track of
+#===================================================================
 admin.site.register(Order)
 admin.site.register(ElectronicTicket)
 admin.site.register(CustomerProfile)
 admin.site.register(LotteryTicket)
 admin.site.register(LotteryDraw)
+admin.site.register(Notification)
